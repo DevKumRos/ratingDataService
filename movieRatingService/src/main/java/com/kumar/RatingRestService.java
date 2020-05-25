b01a8cbff81cd6ecd33e2392b661a420bf530619
kumar22.bms@gmail.com <kumar22.bms@gmail.com>
2020-05-25 17:44:04 +0530

    Updated rating service

---
+++ movieRatingService/src/main/java/com/kumar/RatingRestService.java
@@ -0,0 +1,27 @@
+package com.kumar;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kumar.model.Rating;
+import com.kumar.model.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingRestService {
+
+	
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
